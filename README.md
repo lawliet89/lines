ad03c7b886aacc80c8382e6cee14743676bd27b8
Yong Wen Chua <me@yongwen.xyz>
2017-04-19 14:10:05 +0800

    First commit
    NO TESTS!

---
+++ README.md
@@ -0,0 +1,7 @@
+# lines
+
+Generate many, many lines of repeating text.
+
+## Usage
+
+See `cargo run --release -- --help`
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+extern crate docopt;
+extern crate pbr;
+extern crate rustc_serialize;
+
+use std::cmp;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::iter;
+
+use docopt::Docopt;
+use pbr::ProgressBar;
+
+const USAGE: &'static str = "
+Lines
+
+Generate many many lines of \"hi\"
+
+Usage:
+  lines <count> <file>
+  lines (-h | --help)
+
+Options:
+  -h --help     Show this screen.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_count: usize,
+    arg_file: String,
+}
+
+const ITERATION_SIZE: usize = 65535;
+
+// TODO: Configure string, configure buffer size
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
+
+    let payload = b"hi\n";
+    let buffer: Vec<u8> = iter::repeat(payload)
+        .take(ITERATION_SIZE)
+        .cloned()
+        .flat_map(|s| s.to_vec())
+        .collect();
+    let iterations = args.arg_count % ITERATION_SIZE;
+
+    let file = File::create(&args.arg_file).expect("Cannot create file");
+    let mut writer = BufWriter::with_capacity(buffer.len(), file);
+
+    let mut pb = ProgressBar::new(args.arg_count as u64);
+    pb.format("╢▌▌░╟");
+
+    for iteration in 0..iterations {
+        let start_count = iteration * ITERATION_SIZE;
+        let end_count = cmp::min(start_count + ITERATION_SIZE, args.arg_count);
+        let iteration_count = end_count - start_count;
+        let bytes_count = iteration_count * payload.len();
+
+        writer
+            .write_all(&buffer[0..bytes_count])
+            .expect("Cannot write buffer to file");
+        pb.add(iteration_count as u64);
+    }
+
+    pb.finish_print("Done");
+}
+
+// TODO: Add test
